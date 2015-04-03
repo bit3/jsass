9c7d4c71554c19e5f5811d48c4ed2a96d6cf8dba
Tristan Lins <tristan@lins.io>
2015-04-03 20:02:05 +0200

    Add charset-less StringContext constructor.

--- src/main/java/de/bit3/jsass/context/StringContext.java
+++ src/main/java/de/bit3/jsass/context/StringContext.java
@@ -1,6 +1,7 @@
 package de.bit3.jsass.context;
 
 import de.bit3.jsass.Options;
+import org.apache.commons.io.Charsets;
 
 import java.io.File;
 import java.nio.charset.Charset;
@@ -8,6 +9,10 @@ import java.nio.charset.Charset;
 public class StringContext extends AbstractContext {
     private String  string;
     private Charset charset;
+
+    public StringContext(String string, File inputPath, File outputPath, Options options) {
+        this(string, Charsets.UTF_8, inputPath, outputPath, options);
+    }
 
     public StringContext(String string, Charset charset, File inputPath, File outputPath, Options options) {
         super(inputPath, outputPath, options);
