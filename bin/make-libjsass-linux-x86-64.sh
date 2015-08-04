97e4a850b3c09b510418188b8b9d60880273116d
Tristan Lins <tristan.lins@chamaeleon.de>
2015-08-04 15:28:08 +0200

    Fix BUILD="static" documentation in linux build script.

--- bin/make-libjsass-linux-x86-64.sh
+++ bin/make-libjsass-linux-x86-64.sh
@@ -11,7 +11,7 @@ mkdir -p resources/linux-x86-64
 make -C libsass clean
 
 # We use:
-# - BUILD="shared" to make sure that we build a shared system library
+# - BUILD="static" to make sure that we build a static library
 # - CXX=g++-4.6    to make sure that the library is linked against a version of
 #                  libstdc++.so.6 that is old enough to be widely compatible
 # - CC=gcc-4.4     same as for CXX but for libc.so.6
