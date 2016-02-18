00c8ef2c1581f89a74b0543aeb8dd05e2c270451
Lars Grefer <lars.grefer@materna.de>
2016-02-18 16:33:09 +0100

    Throw Exceptions on compile errors

--- src/main/java/io/bit3/jsass/CompilationException.java
+++ src/main/java/io/bit3/jsass/CompilationException.java
@@ -2,14 +2,56 @@ package io.bit3.jsass;
 
 public class CompilationException extends Exception {
 
-  private int status;
+  private Output output;
 
-  public CompilationException(int status, String message) {
-    super(message);
-    this.status = status;
+  public CompilationException(Output output) {
+    super(output.getErrorText());
+    this.output = output;
   }
 
-  public int getStatus() {
-    return status;
+  public Output getOutput() {
+    return output;
+  }
+
+  /**
+   * @see Output#getErrorStatus()
+   */
+  public int getErrorStatus() {
+    return getOutput().getErrorStatus();
+  }
+
+  /**
+   * @see Output#getErrorJson()
+   */
+  public String getErrorJson() {
+    return getOutput().getErrorJson();
+  }
+
+  /**
+   * @see Output#getErrorText()
+   */
+  public String getErrorText() {
+    return getOutput().getErrorText();
+  }
+
+  /**
+   * @see Output#getErrorMessage()
+   */
+  public String getErrorMessage() {
+    return getOutput().getErrorMessage();
+  }
+
+  /**
+   * @see Output#getErrorFile()
+   */
+  public String getErrorFile() {
+    return getOutput().getErrorFile();
+  }
+
+  /**
+   * @see Output#getErrorSrc()
+   */
+  public String getErrorSrc() {
+    return getOutput().getErrorSrc();
   }
 }
