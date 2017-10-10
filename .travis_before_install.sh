629059c554a3f75db8aa2c806d0ab681968fcbf4
Tristan Lins <tristan@lins.io>
2017-10-10 19:31:51 +0200

    Upgrade oracle jdk for travis macos test

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -17,10 +17,10 @@ fi
 if [[ "osx" == $TRAVIS_OS_NAME ]]; then
     wget -nv --no-cookies --no-check-certificate \
          --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-         "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-macosx-x64.dmg" \
-         -O jdk-8u131-macosx-x64.dmg
-    hdiutil mount jdk-8u131-macosx-x64.dmg
-    sudo installer -package "/Volumes/JDK 8 Update 131/JDK 8 Update 131.pkg" -target "/"
+         "http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-macosx-x64.dmg" \
+         -O jdk-8u144-macosx-x64.dmg
+    hdiutil mount jdk-8u144-macosx-x64.dmg
+    sudo installer -package "/Volumes/JDK 8 Update 131/JDK 8 Update 144.pkg" -target "/"
 fi
 
 git fetch --unshallow
