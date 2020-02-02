68d60b6d77ffddda5ee8a62feca82bcdeb7bf8df
Tristan Lins <tristan@lins.io>
2020-02-02 17:15:49 +0100

    [CI] Use AdoptOpenJDK to run tests on windows and macos

--- .travis_script.sh
+++ .travis_script.sh
@@ -3,10 +3,10 @@ set -ev
 
 if [[ "windows" == $TRAVIS_OS_NAME ]]; then
     wget -nv --no-cookies --no-check-certificate \
-         "https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip" \
-         -O openjdk-11.0.2_windows-x64_bin.zip
-    unzip -x "openjdk-11.0.2_windows-x64_bin.zip"
-    export JAVA_HOME="$(pwd)/jdk-11.0.2"
+         "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.6%2B10/OpenJDK11U-jdk_x64_windows_hotspot_11.0.6_10.zip" \
+         -O OpenJDK11U-jdk_x64_windows_hotspot_11.0.6_10.zip
+    unzip -x "OpenJDK11U-jdk_x64_windows_hotspot_11.0.6_10.zip"
+    export JAVA_HOME="$(pwd)/jdk-11.0.6+10"
     export PATH="$JAVA_HOME/bin:$PATH"
     echo "Java Version"
     java -version
@@ -15,10 +15,11 @@ fi
 
 if [[ "osx" == $TRAVIS_OS_NAME ]]; then
     wget -nv --no-cookies --no-check-certificate \
-         "https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_osx-x64_bin.tar.gz" \
-         -O openjdk-11.0.2_osx-x64_bin.tar.gz
-    tar xf "openjdk-11.0.2_osx-x64_bin.tar.gz"
-    sudo mv jdk-11.0.2.jdk /Library/Java/JavaVirtualMachines/
+         "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.6%2B10/OpenJDK11U-jdk_x64_mac_hotspot_11.0.6_10.tar.gz" \
+         -O OpenJDK11U-jdk_x64_mac_hotspot_11.0.6_10.tar.gz
+    tar xf "OpenJDK11U-jdk_x64_mac_hotspot_11.0.6_10.tar.gz"
+    sudo mv jdk-11.0.6+10 /Library/Java/JavaVirtualMachines/
+    export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-11.0.6+10/Contents/Home"
     echo "Java Version"
     java -version
     javac -version
